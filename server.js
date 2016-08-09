5edbb1f8b30281c02fd83e10d5b0aec534255f58
Joey Reyes <jreyes88@gmail.com>
2016-08-09 10:55:32 -0500

    cleaning up server.js

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
+// Dependencies
+// ===============================================
 var express = require('express');
 var app = express();
-
-// /-/-/-/-/-/-/-/-/-/-//-/-/-/-/-/-/-/-/-/-/
-// dependencies
-// /-/-/-/-/-/-/-/-/-/-//-/-/-/-/-/-/-/-/-/-/
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
+
+// (is this a typo? should it be multipart?)
 var mutilpart = require('connect-multiparty');
+// ()
+
 var uploader = require('express-fileuploader');
 var mysql = require('mysql');
 
-// load stylesheets, imgs, etc.
-// app.use(express.static(process.cwd() + '/public'));
+// Load stylesheets, imgs, etc. from static folder
+// ===============================================
+app.use(express.static(process.cwd() + '/static'));
 
-// parse incoming responses into body
+// Parse incoming responses into body
+// ===============================================
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-// adds a static file server.
-app.use(express.static(process.cwd() + '/static'));
 
-// hack the form methods to be more than GET and POST
+// Method Override allows for deleting/updating info from the db
+// ===============================================
 app.use(methodOverride('_method'));
 
 // sets the 'main.handlebars' file to be the default
+// ===============================================
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-// reference the 'burgers_controller.js' file to handle all the routing endpoints
+// Route to the controller
+// ===============================================
 var routes = require('./controllers/classify-controller.js');
 app.use('/', routes);
 
 
 // set up the Express Server (app) to listen using the 'process.env.PORT' value (aka, the one Heroku assigns, I assume) or PORT 8000
+// ===============================================
 app.listen(3000, function() {
     console.log("server listening on port: " + 3000);
 });
