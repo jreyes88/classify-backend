0d3be7f5bba1686cb5761bda99d626500c023da0
dsuare1 <suarez.derrick@gmail.com>
2016-08-09 00:36:35 -0500

    adding experimental code for authenticating users (using express middleware)

--- server.js
+++ server.js
@@ -37,3 +37,42 @@ app.use('/', routes);
 app.listen(process.env.PORT || 3001, function() {
     console.log("server listening on port: " + process.env.PORT);
 });
+
+
+
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+// one way to implement middleware for authenticating users w/ login
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+
+
+// function checkAuth(req, res, next) {
+//   if (!req.session.user_id) {
+//     res.send('You are not authorized to view this page');
+//   } else {
+//     next();
+//   }
+// }
+
+
+// // any route that requires a login authentication
+// app.get('/my_secret_page', checkAuth, function (req, res) {
+//   res.send('if you are viewing this page it means you are logged in');
+// });
+
+// // login route
+// app.post('/login', function (req, res) {
+//   var post = req.body;
+//   if (post.user === 'john' && post.password === 'johnspassword') {
+//     req.session.user_id = johns_user_id_here;
+//     res.redirect('/my_secret_page');
+//   } else {
+//     res.send('Bad user/pass');
+//   }
+// });
+
+// // logout route
+// app.get('/logout', function (req, res) {
+//   delete req.session.user_id;
+//   res.redirect('/login');
+// });
