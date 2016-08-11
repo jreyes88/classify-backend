cf5a74a82382c0eaaf8fdc8b02f182e81c3c76e9
dsuare1 <suarez.derrick@gmail.com>
2016-08-10 21:49:13 -0500

    updated db files

--- migrations/20160809013455-create-user-page.js
+++ migrations/20160809013455-create-user-page.js
@@ -11,6 +11,9 @@ module.exports = {
             title: {
                 type: Sequelize.STRING
             },
+            userID: {
+                type: Sequelize.INTEGER
+            },
             domain: {
                 type: Sequelize.STRING
             },
