6126a2e0b5b2690190debc7a65e652e748bb5f28
chanceeakin <chance.eakin@gmail.com>
2016-08-12 17:48:30 -0500

    backend m.v.p.

--- controllers/userGeneratedPage-controller.js
+++ controllers/userGeneratedPage-controller.js
@@ -1,30 +1,58 @@
-module.exports = function(app, models)
-	{
-	// 	app.get('/:username', function(req, res)
-	// 		{
-	// 			var paramsUser = req.params.username;
-	// 			console.log("params user ==== " + JSON.stringify(paramsUser, null, 2));
-	// 			// var activeUser;
-	// 			console.log(req.params);
-	// 			models.userID.findOne(
-	// 				{
-	// 					where:
-	// 						{
-	// 							username: paramsUser
-	// 						}
-	// 				}
-	// 			).then(function(res)
-	// 				{
-	// 					// activeUser = res;
-	// 					console.log("Test Stuff =========== " + JSON.stringify(res));
-	// 				}
-	// 			)
-	// 	// models.userContent.create({
+module.exports = function(app, models) {
+    app.get('/:username/:title', function(req, res) {
+        var paramsUser = req.params.username;
+        var userID;
+        var pageID;
+        var paramsTitle = req.params.title;
+        var hbsObj = {
+            pageName: '',
+            username: '',
+            pOneName: '',
+            pOneData: '',
+            pOneDataType: '',
+            pTwoName: '',
+            pTwoData: '',
+            pTwoDataType: '',
+            pThreeName: '',
+            pThreeData: '',
+            pThreeDataType: '',
+            pFourName: '',
+            pFourData: '',
+            pFourDataType: '',
+        }
+        models.userID.findOne({ where: { username: paramsUser } }).then(function(res) {
+            userID = res.id;
+            hbsObj.username = res.username;
+            console.log("USER NAME!!!!!    " + hbsObj.username);
+        }).then(function() {
+            models.userPage.findOne({ where: { userId: userID, title: paramsTitle } }).then(function(res) {
+                pageID = res.id
+            }).then(function() {
+                models.userContent.findOne({ where: { pageId: pageID,pagePosition: 1 } }).then(function(res) {
+                		hbsObj.pOneName = res.name;
+                		hbsObj.pOneData = res.data;
+                		hbsObj.pOneDataType = res.dataType;
+	                });
+                models.userContent.findOne({ where: { pageId: pageID,pagePosition: 2 } }).then(function(res) {
+                		hbsObj.pTwoName = res.name;
+                		hbsObj.pTwoData = res.data;
+                		hbsObj.pTwoDataType = res.dataType;
+	                });
+                models.userContent.findOne({ where: { pageId: pageID,pagePosition: 3 } }).then(function(res) {
+                		hbsObj.pThreeName = res.name;
+                		hbsObj.pThreeData = res.data;
+                		hbsObj.pThreeDataType = res.dataType;
+	                });
+                models.userContent.findOne({ where: { pageId: pageID,pagePosition: 4 } }).then(function(res) {
+                		hbsObj.pFourName = res.name;
+                		hbsObj.pFourData = res.data;
+                		hbsObj.pFourDataType = res.dataType;
+                		console.log(hbsObj);
+	                });
+            }).then(function(){
+            	res.render('student', hbsObj);
+            })
+        })
 
-	// 	// })
-
-	// 	// models.userPage.create({
-
-	// 	// })
-	// });
+    });
 };
