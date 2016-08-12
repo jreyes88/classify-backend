09b4ead141b928572210bcd12ab2ab61cf2040f5
Joey Reyes <jreyes88@gmail.com>
2016-08-12 16:58:07 -0500

    trying to fix merge conflicts

--- controllers/addContent-controller.js
+++ controllers/addContent-controller.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-userID = require('../models/').userID;
-userPages = require('../models/').userpage;
-
-// any route that requires a login authentication
- module.exports = function(app, models) {
-    console.log('addContent controller loaded.');
-
-    var userID;
-    var domain;
-    var pageID;
-    var pageName;
-
-    app.post('/addcontent', function(req, res) {
-        var data = JSON.stringify(req.body);
-        pageName = req.body.pageName;
-        models.userID.findOne({ where: { username: req.body.username } }).then(function(res) {
-            userID = res.id;
-            domain = res.domain;
-        }).then(function() {
-            models.userPage.create({
-                title: req.body.pageName,
-                userID: userID,
-                domain: domain,
-                template: "student"
-            })
-        }).then(function() {
-            // this fires after the userPage.create. find a way to make it fire synchronously.
-            models.userPage.findOne({
-                where: {
-                    userID: userID,
-                    title: pageName
-                }
-            })
-        }).then(function(res) {
-            console.log(res);
-            pageID = res.id;
-        })
-    })
-};
-=======
  // any route that requires a login authentication
  module.exports = function(app, models) {
      console.log('addContent controller loaded.');
@@ -98,4 +57,3 @@ userPages = require('../models/').userpage;
      })
 
  };
->>>>>>> 950df82e7d52179047161f87bd811546ac97688e
--- controllers/userGeneratedPage-controller.js
+++ controllers/userGeneratedPage-controller.js
@@ -1,30 +1,9 @@
 module.exports = function(app, models)
 	{
-	// 	app.get('/:username', function(req, res)
-	// 		{
-	// 			var paramsUser = req.params.username;
-	// 			console.log("params user ==== " + JSON.stringify(paramsUser, null, 2));
-	// 			// var activeUser;
-	// 			console.log(req.params);
-	// 			models.userID.findOne(
-	// 				{
-	// 					where:
-	// 						{
-	// 							username: paramsUser
-	// 						}
-	// 				}
-	// 			).then(function(res)
-	// 				{
-	// 					// activeUser = res;
-	// 					console.log("Test Stuff =========== " + JSON.stringify(res));
-	// 				}
-	// 			)
-	// 	// models.userContent.create({
-
-	// 	// })
-
-	// 	// models.userPage.create({
-
-	// 	// })
-	// });
-};
+		app.get('/:username/:title', function(req, res)
+			{
+				var paramsUser = req.params.username;
+				var paramsTitle = req.params.title;
+				console.log(req.params);
+			});
+	};
