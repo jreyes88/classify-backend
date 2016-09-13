4574aa3dd62ee367e9ce4319c7997bc7e7882bd2
Joey Reyes <jreyes88@gmail.com>
2016-09-12 22:06:24 -0500

    test

--- controllers/addContent-controller.js
+++ controllers/addContent-controller.js
@@ -1,57 +1,81 @@
-// any route that requires a login authentication
- module.exports = function(app, models) {
-     console.log('addContent controller loaded.');
+module.exports = function(app, models)
+    {
+        console.log('addContent controller loaded.');
+        var userID;
+        var domain;
+        var pageID;
+        var pageName;
 
-     var userID;
-     var domain;
-     var pageID;
-     var pageName;
-
-     app.post('/addcontent', function(req, res) {
-         console.log(req.body.headerData);
-         var data = req.body;
-         models.userID.findOne({ where: { username: req.body.username } }).then(function(res) {
-             userID = res.id;
-             domain = res.domain;
-         }).then(function() {
-             models.userPage.create({
-                 title: req.body.pageName,
-                 userId: userID,
-                 domain: domain,
-                 template: "student"
-             }).then(function(res) {
-                 pageID = res.dataValues.id;
-             }).then(function() {
-                 models.userContent.create({
-                     name: data.headerName,
-                     data: data.headerData,
-                     dataType: data.headerDataType,
-                     pageId: pageID,
-                     pagePosition: data.headerPagePosition
-                 });
-                 models.userContent.create({
-                     name: data.calendarName,
-                     data: data.calendarData,
-                     dataType: data.calendarDataType,
-                     pageId: pageID,
-                     pagePosition: data.calendarPagePosition
-                 });
-                 models.userContent.create({
-                     name: data.youTubeName,
-                     data: data.youTubeData,
-                     dataType: data.youTubeDataType,
-                     pageId: pageID,
-                     pagePosition: data.youTubePagePosition,
-                 });
-                 models.userContent.create({
-                     name: data.textName,
-                     data: data.textData,
-                     dataType: data.textDataType,
-                     pageId: pageID,
-                     pagePosition: data.textPagePosition
-                 });
-             })
-         })
-     })
-
- };
+        app.post('/addcontent', function(req, res)
+            {
+                var data = req.body;
+                models.userID.findOne(
+                    {
+                        where:
+                            {
+                                username: req.body.username
+                            }
+                    }
+                ).then(function(res)
+                    {
+                        userID = res.id;
+                        domain = res.domain;
+                    }
+                ).then(function()
+                    {
+                        models.userPage.create(
+                            {
+                                title: req.body.pageName,
+                                userId: userID,
+                                domain: domain,
+                                template: "student"
+                            }
+                        ).then(function(res)
+                            {
+                                pageID = res.dataValues.id;
+                            }
+                        ).then(function()
+                            {
+                                models.userContent.create(
+                                    {
+                                        name: data.headerName,
+                                        data: data.headerData,
+                                        dataType: data.headerDataType,
+                                        pageId: pageID,
+                                        pagePosition: data.headerPagePosition
+                                    }
+                                );
+                                models.userContent.create(
+                                    {
+                                        name: data.calendarName,
+                                        data: data.calendarData,
+                                        dataType: data.calendarDataType,
+                                        pageId: pageID,
+                                        pagePosition: data.calendarPagePosition
+                                    }
+                                );
+                                models.userContent.create(
+                                    {
+                                        name: data.youTubeName,
+                                        data: data.youTubeData,
+                                        dataType: data.youTubeDataType,
+                                        pageId: pageID,
+                                        pagePosition: data.youTubePagePosition,
+                                    }
+                                );
+                                models.userContent.create(
+                                    {
+                                        name: data.textName,
+                                        data: data.textData,
+                                        dataType: data.textDataType,
+                                        pageId: pageID,
+                                        pagePosition: data.textPagePosition
+                                    }
+                                );
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    };
--- controllers/classify-controller.js
+++ controllers/classify-controller.js
@@ -1,13 +1,12 @@
-var express = require('express'),
-models = require('../models'),
-router = express.Router(),
-loginController = require('./login-controller.js'),
-contentController = require('./addContent-controller.js'),
-adminController = require('./admin-controller.js'),
-userGeneratedPageController = require('./userGeneratedPage-controller.js'),
-userIDController = require('./userID-controller.js'),
-userPageController = require('./userPage-controller.js'),
-userContentController = require('./userContent-controller.js');
+var 	express							=	require('express'),
+		models							=	require('../models'),
+		Router							=	express.Router(),
+		loginController					=	require('./login-controller.js'),
+		contentController 				=	require('./addContent-controller.js'),
+		userGeneratedPageController 	=	require('./userGeneratedPage-controller.js'),
+		userIDController 				=	require('./userID-controller.js'),
+		userPageController 				=	require('./userPage-controller.js'),
+		userContentController 			=	require('./userContent-controller.js');
 
 
 loginController(router, models);
@@ -15,18 +14,11 @@ contentController(router, models);
 adminController(router, models);
 userGeneratedPageController(router, models);
 
-// router.get('/', function(req, res) {
-//     res.redirect('/classify');
-// });
-
-router.get('/', function(req, res) {
-	res.render('index');
-})
+router.get('/', function(req, res)
+	{
+		res.render('index');
+	}
+);
 
 console.log('Main router loaded.');
-
-// router.get('/createDomain', function(req, res) {
-// 	res.render('./partials/domSubmission');
-// });
-
 module.exports = router;
